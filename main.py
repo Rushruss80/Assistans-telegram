30dab127c2e746828cb7a60271c8de4a356ff4a6
Rushruss80 <rushrus80@gmail.com>
2025-05-17 14:49:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def handle_task(message):
         tasks[user_id] = []
 
     tasks[user_id].append({"task": text, "time": now, "done": False})
-    bot.send_message(user_id, f"Задачу записано: '{text}'")
-"Я нагадаю тобі пізніше ")
+    bot.send_message(user_id, f"Задача прийнята: {text}")
+    bot.send_message(user_id,"Я нагадаю тобі пізніше ")
 
 bot.polling()
