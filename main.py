aa22f6a2171e9f45aa745c4372f4e340eb6a46de
Rushruss80 <rushrus80@gmail.com>
2025-05-17 15:26:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import telebot
 from datetime import datetime
 
-BOT_TOKEN = os.getenv("BOT_TOKEN").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN").replace('–', '-').replace('—', '-').strip()
 bot = telebot.TeleBot(BOT_TOKEN)
 
 tasks = {}
